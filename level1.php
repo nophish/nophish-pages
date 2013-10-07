5b58465593d6b811299c8058bda3bd219c052e65
Clemens Bergmann <clb@no-phish.de>
2013-10-07 15:52:10 +0200

    add webService and pages to git

---
+++ level1.php
@@ -0,0 +1,10 @@
+<html>
+			<head>
+				<title>Anti Phishing Education</title>
+			</head>
+			<body>
+			  <p>Sehr geehrter Nutzer,</p>
+			  <p>Dies ist das Erste Level. Zur Zeit besteht es nur aus einem Link den des zu clicken gilt.</p>
+			  <p><a href="phishedu://level1finished/">Bitte hier clicken</a></p>
+			</body>
+		</html>
