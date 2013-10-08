a7677261b46d83d31036630cb6879248f883ea79
Clemens Bergmann <clb@no-phish.de>
2013-10-08 09:57:44 +0200

    make the level1 page very long

--- level1.php
+++ level1.php
@@ -6,5 +6,7 @@
 			  <p>Sehr geehrter Nutzer,</p>
 			  <p>Dies ist das Erste Level. Zur Zeit besteht es nur aus einem Link den des zu clicken gilt.</p>
 			  <p><a href="phishedu://level1finished/">Bitte hier clicken</a></p>
+                          <div style="height:110%;border: 1px solid red;">This is very long</div>
+                          <a name="bottom" />
 			</body>
 		</html>
