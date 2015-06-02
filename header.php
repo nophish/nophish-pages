32cf25f2d5e8ea1077b58a87f4d64792eb0c0c24
Clemens Bergmann <cbergmann@schuhklassert.de>
2015-06-02 13:05:29 +0200

    fine tune redirect

--- header.php
+++ header.php
@@ -12,7 +12,7 @@ if($title=""){
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <link href="css/style.css" rel="stylesheet">
     <?php if($redirect!=""){ ?>
-    <meta http-equiv="refresh" content="10; url=<?php echo $redirect; ?>" />
+    <meta http-equiv="refresh" content="5; url=<?php echo $redirect; ?>" />
     <?php } ?>
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
--- index.php
+++ index.php
@@ -3,6 +3,7 @@ $redirect="https://www.secuso.informatik.tu-darmstadt.de/de/research/results/nop
 include("header.php");
 ?>
   <p>Diese Webadresse ist zum internen Gebrauch f&uuml;r die <a href="https://play.google.com/store/apps/details?id=de.tudarmstadt.informatik.secuso.phishedu">Anti Phishing Education App</a> welche im Rahmen der Master Arbeiten von Gamze Canova und Clemens Bergmann im Fachgebiet <a href="https://www.secuso.informatik.tu-darmstadt.de">secuso</a> der <a href="https://www.tu-darmstadt.de">Technischen Universit&auml;t Darmstadt</a> angefertigt wurde.</p>
+<p>Sie werden jetzt auf die Seite von SeCuSo umgeleitet.</p>
   <p>
     Zum Kontakt zu unseren Benutzer Pflegen wir &uuml;ber unsere <a href="https://plus.google.com/104924310794712066136" rel="publisher">Google+ Seite</a>
   </p>
